c038ae080358914541cfcd15b3051fae959a727c
Atena <aten0004@edu.easj.dk>
2016-11-11 12:22:45 +0100

    cvsdff

---
+++ hotel1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("It is just a test");
+        }
+    }
+}
